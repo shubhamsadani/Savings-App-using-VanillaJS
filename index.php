e49910bf6d8e1142e291f8d3d423524edbfa7234
Shubham Sadani <shubham.sadani@somaiya.edu>
2022-01-14 14:38:24 +0530

    Updated bottom margin of the page

--- index.php
+++ index.php
@@ -83,12 +83,12 @@
             
         </div>
         <!-- <div class="card">
-        <div class="card-body py-5">
+        <div class="card-body">
             <h5 class="card-title text-primary">Apple</h5>
             <p class="card-text text-success">25/11/20, Wednesday</p>
         </div>
         </div> -->
-        <ul class="list-group" id="output"></ul>
+        <ul class="list-group mb-3" id="output"></ul>
     </div>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
